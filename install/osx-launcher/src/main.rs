6bd1965ada2f020dd3bab46d28a6d8f335b0f909
Micah Lee <micah@micahflee.com>
2016-11-02 12:04:40 -0700

    Improve the OSX launcher, so it can set environment variables and launch the right binary, though not tested yet

--- install/osx-launcher/src/main.rs
+++ install/osx-launcher/src/main.rs
@@ -1,7 +1,23 @@
 use std::process::Command;
+use std::env::current_exe;
 
 fn main() {
-    Command::new("/Applications/Calculator.app/Contents/MacOS/Calculator")
+    Command::new(build_path("env/bin/python3"))
+        // Set the environment variables
+        .env("PATH", build_path("env/bin"))
+        .env("PYTHONHOME", build_path("env"))
+        .env("PYTHONPATH", build_path("env/lib/python3.5"))
+        // Set the arguements
+        .arg("-c")
+        .arg("import gpgsync; gpgsync.main()")
         .spawn()
         .expect("failed to execute process");
+}
+
+fn build_path(filename: &str) -> String {
+    // Find the path inside the app bundle of Contents/MacOS
+    let mut path = current_exe().unwrap();
+    path.pop(); // chop off gpgsync, the name of the launcher
+    path.push(filename);
+    String::from(path.to_str().unwrap())
 }
